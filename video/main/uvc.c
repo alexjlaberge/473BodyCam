5a59ef033705d843af1e86fb0fd28876129c49da
Alec Ten Harmsel <alec@alectenharmsel.com>
2015-11-01 10:45:06 -0500

    Initial implementation of uvc_open

--- video/main/uvc.c
+++ video/main/uvc.c
@@ -8,13 +8,23 @@
 #include "uvc.h"
 #include <usblib/usblib.h>
 #include <usblib/host/usbhost.h>
+#include <usblib/host/usbhostpriv.h>
 
-#define UVC_CTRL_ENDPOINT 0
+/* TODO Add more members as necessary */
+struct camera_device
+{
+	tUSBHostDevice *device;
+	uint8_t in_use;
+	uint32_t interrupt_in_pipe;
+};
+
+static struct camera_device cam_inst = {0, 0};
 
 static void * uvc_open(tUSBHostDevice *dev);
 static void uvc_close(void *dev);
 static uint32_t uvc_callback(void *camera, uint32_t event, uint32_t msg_param,
 		void *msg_data);
+static void uvc_int_in_callback(uint32_t pipe, uint32_t event);
 
 const tUSBHostClassDriver uvc_driver =
 {
@@ -24,12 +34,58 @@ const tUSBHostClassDriver uvc_driver =
     0
 };
 
-void * uvc_open(tUSBHostDevice *dev)
+static void * uvc_open(tUSBHostDevice *dev)
 {
+	tInterfaceDescriptor *iface;
+	tEndpointDescriptor *endpoint_desc;
+	int i;
+
+	iface = USBDescGetInterface(dev->psConfigDescriptor, 0, 0);
+
+	if (!cam_inst.in_use)
+	{
+		cam_inst.device = dev;
+		cam_inst.in_use = 1;
+
+		for (i = 0; i < iface->bNumEndpoints; i++)
+		{
+			endpoint_desc = USBDescGetInterfaceEndpoint(iface, i, 256);
+			if (endpoint_desc == 0)
+			{
+				break;
+			}
+
+			/* TODO actually set up the proper endpoints */
+			/* TODO we are using isochronous transfers, not interrupts/bulk */
+
+			if ((endpoint_desc->bmAttributes & USB_EP_ATTR_TYPE_M) ==
+					USB_EP_ATTR_INT)
+			{
+				if (endpoint_desc->bEndpointAddress & USB_EP_DESC_IN)
+				{
+					cam_inst.interrupt_in_pipe = USBHCDPipeAlloc(0,
+							USBHCD_PIPE_INTR_IN, dev, uvc_int_in_callback);
+					USBHCDPipeConfig(cam_inst.interrupt_in_pipe,
+							endpoint_desc->wMaxPacketSize,
+							endpoint_desc->bInterval,
+							endpoint_desc->bEndpointAddress & USB_EP_DESC_NUM_M
+							);
+				}
+			}
+		}
+
+		return &cam_inst;
+	}
+
 	return (void *) 0;
 }
 
-void uvc_close(void *dev)
+static void uvc_close(void *dev)
+{
+	return;
+}
+
+static void uvc_int_in_callback(uint32_t pipe, uint32_t event)
 {
 	return;
 }
